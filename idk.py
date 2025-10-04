2b8b1f4aaa30028ab130464e0ba7bf31fe7a28ad
fperone <148684728+fperone@users.noreply.github.com>
2025-10-04 10:41:53 -0300

    Update naivebayes.py

--- idk.py
+++ idk.py
@@ -1,5 +1,11 @@
-import csv
 import pandas as pd
-x = input("Type the dataset filepath: ")
 df = pd.read_csv("Multi-Labelled_Smiles_Odors_dataset.csv")
-print(df.head())  # mostra as primeiras linhas
+dicio = {}
+smiles = df.iloc[:,0].tolist()
+descritores = df.iloc[:,1].tolist()
+if len(descritores) == len(smiles):
+    print(len(descritores))
+    print("True")
+for nmr in range(len(smiles)):
+    dicio[smiles[nmr]] = tuple(descritores[nmr].split(";"))
+print(dicio)
