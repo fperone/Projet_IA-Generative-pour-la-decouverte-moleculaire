bfd4081c5bfe1b2ef4d9bddcaed1aefc433ea7dd
fperone <148684728+fperone@users.noreply.github.com>
2025-10-04 08:57:01 -0300

    Update idk.py

--- idk.py
+++ idk.py
@@ -1,5 +1,5 @@
 import csv
 import pandas as pd
 x = input("Type the dataset filepath: ")
-df = pd.read_csv("x")
+df = pd.read_csv("Multi-Labelled_Smiles_Odors_dataset.csv")
 print(df.head())  # mostra as primeiras linhas
